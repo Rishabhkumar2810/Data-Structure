fd3f1878c9f3b7fb66960b1c632c3547e41c1b4c
Rishabh Kumar <rishabhporwal6397@gmail.com>
2026-09-13 02:40:56 +0530

    Add LeetCode #1 - Two Sum

---
+++ solutions/Arrays/0001-two-sum/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int>mp;
+       for (int i = 0; i < nums.size(); i++) 
+       {
+           int a=nums[i];
+           int b=target-a;
+           if(mp.find(b)!=mp.end())
+            return {mp[b], i};
+            mp[a]=i;    
+            
+        }
+        return {}; 
+        
+    }
+};
