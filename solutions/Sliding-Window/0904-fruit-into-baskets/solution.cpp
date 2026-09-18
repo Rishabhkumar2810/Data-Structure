25b87d776826ca96b1a85f68ef3df7d598a5ea2b
Rishabh Kumar <rishabhporwal6397@gmail.com>
2026-09-18 18:46:46 +0530

    Add LeetCode #904 - Fruit Into Baskets

---
+++ solutions/Sliding-Window/0904-fruit-into-baskets/solution.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+       int n=fruits.size();
+       int l=0;
+       int r=0;
+       int ans=0;
+       unordered_map<int,int>mp;
+       while(r<n)
+       {
+        mp[fruits[r]]++;
+        if(mp.size()>2)
+        {
+            while(mp.size()>2)
+            {
+                mp[fruits[l]]--;
+                if(mp[fruits[l]]==0)
+                mp.erase(fruits[l]);
+                l++;
+            }
+        }
+        if(mp.size()<=2)
+        {
+            ans=max(ans,r-l+1);
+        }
+        r++;
+       }
+       return ans;
+        
+    }
+};
